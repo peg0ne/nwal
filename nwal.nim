f0faead88fb91c2f78fe1e074a206870a86c5f42
peg0ne <54445091+peg0ne@users.noreply.github.com>
2022-02-18 13:27:16 +0100

    Update nwal.nim

--- nwal.nim
+++ nwal.nim
@@ -1,8 +1,8 @@
 import nimpy
   
 const BASE: array[8, array[3, int]] = [[0,0,0], [255,0,0], [0,255,0], [0,0,255], [255, 255, 0], [255, 0, 255], [0, 255, 255], [255,255,255]]
-const MINBRIGHTNESS: int = 255
-const MINDARKNESS: int = 5
+const MINBRIGHTNESS: int = 500
+const MINDARKNESS: int = 35
 
 proc abs(val: int): int =
   if val < 0:
@@ -17,16 +17,10 @@ proc checkDistance(vec: array[3, int], vec2: array[3, int]): int =
 
 proc darken(color: array[3, int]): array[3, int] =
   var normalizedColor: array[3, int]
-  var mainColor = 0
   for i,n in color:
-    if color[mainColor] > color[i]:
-      mainColor = i
     normalizedColor[i] = color[i]
   while normalizedColor[0] + normalizedColor[1] + normalizedColor[2] > MINDARKNESS:
     for i in 0..2:
-      if i == mainColor:
-        normalizedColor[i] -= 1
-      else:
         normalizedColor[i] -= 2
   return normalizedColor
 
@@ -34,23 +28,23 @@ proc normalize(color: array[3, int]): array[3, int] =
   var normalizedColor: array[3, int]
   var mainColor = 0
   for i,n in color:
-    if color[mainColor] < color[i]:
+    if color[mainColor] > color[i]:
       mainColor = i
     normalizedColor[i] = color[i]
 
   while normalizedColor[0] + normalizedColor[1] + normalizedColor[2] > MINBRIGHTNESS:
     for i in 0..2:
       if i == mainColor:
-        normalizedColor[i] -= 1
+        normalizedColor[i] -= 3
       else:
-        normalizedColor[i] -= 2
+        normalizedColor[i] -= 1
 
   while normalizedColor[0] + normalizedColor[1] + normalizedColor[2] < MINBRIGHTNESS:
     for i in 0..2:
       if i == mainColor:
-        normalizedColor[i] += 2
-      else:
         normalizedColor[i] += 1
+      else:
+        normalizedColor[i] += 3
   return normalizedColor
 
 proc normalizeBrightness(vec: array[8, array[3, int]]): array[8, array[3, int]] =
@@ -72,12 +66,11 @@ proc extract(px: seq[array[3, int]]): array[8, array[3, int]] {.exportpy.} =
   var i = 0
   while i < px.len():
     for j,c in colors:
-      if c[0] == 0 and c[1] == 0 and c[2] == 0:
+      if px[i][0] + px[i][1] + px[i][2] < 255 or px[i][0] + px[i][1] + px[i][2] > 600:
+        continue
+      elif c[0] == 0 and c[1] == 0 and c[2] == 0:
         colors[j] = px[i]
       elif checkDistance(BASE[j], c) > checkDistance(BASE[j], px[i]):
         colors[j] = px[i]
     i += 10
   return normalizeBrightness(colors)
-
-      
-
